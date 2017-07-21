caa87b44226f958c809facd82b3230f8a8a6008d
wbdana <william.b.dana@gmail.com>
2017-07-21 13:16:41 -0400

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,48 @@
+class CrewMember{
+    constructor(position){
+      this.position = position;
+      this.currentShip = 'Looking for a Rig';
+    }
 
+    checkForShip(){
+      if(this.currentShip === 'Looking for a Rig'){
+        return false
+      } else {
+        return true
+      }
+    }
+
+    setShip(ship){
+      this.currentShip = ship;
+    }
+
+    engageWarpDrive(){
+      if(this.checkForShip()){
+        if(this.position === 'Pilot'){
+          this.currentShip.warpDrive = 'engaged!';
+        }
+      } else {
+        return 'had no effect'
+      }
+    }
+
+    setsInvisibility(){
+      if(this.checkForShip()){
+        if(this.position === 'Defender'){
+          this.currentShip.cloaked = true;
+        }
+      } else {
+        return 'had no effect'
+      }
+    }
+
+    chargePhasers(){
+      if(this.checkForShip()){
+        if(this.position === 'Gunner'){
+          this.currentShip.phasersCharge = 'charged!';
+        }
+      } else {
+        return 'had no effect'
+      }
+    }
+  }
